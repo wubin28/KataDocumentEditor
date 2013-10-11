9296dc1e25f474bb5f0ec647f88b4dccc11f4344
Ben Wu <wubin28@gmail.com>
2013-10-11 14:22:53 +0800

    Added constructor and setColor() method to DocumentEditorFacade.

--- test/DocumentEditor/DocumentEditorFacade.h
+++ test/DocumentEditor/DocumentEditorFacade.h
@@ -5,13 +5,23 @@
 
 class DocumentEditorFacade {
 public:
+    DocumentEditorFacade(unsigned rowMax = 0, unsigned colMax = 0, std::string s = ""):
+        rowMax(rowMax), colMax(colMax), contents(s) {
+    }
+    void setColor(unsigned rowIndex, unsigned colIndex, unsigned length, std::string color) {
+    }
+
+    // Hello world demo.
     int sayOne() {
         return 1;
     }
-
     std::string sayHello() {
         return "Hello!";
     }
+private:
+    unsigned rowMax;
+    unsigned colMax;
+    std::string contents;
 };
 
 #endif
--- test/DocumentEditor/DocumentEditorFacadeTest.cpp
+++ test/DocumentEditor/DocumentEditorFacadeTest.cpp
@@ -2,16 +2,34 @@
 #include "CppUnitLite/TestDefaultConfig.h"
 #include "DocumentEditorFacade.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	CommandLineTestRunner::RunAllTests(argc, argv);
 	return 0;
 }
 
-TEST( DocumentEditorFacade, creation)
-{
+TEST( DocumentEditorFacade, creation) {
+    // Given
     DocumentEditorFacade documentEditorFacade;
+
+    // When & Then
     CHECK ( 1 == documentEditorFacade.sayOne() );
     LONGS_EQUAL ( 1 , documentEditorFacade.sayOne() );
     CHECK ( "Hello!" == documentEditorFacade.sayHello() );
+}
+
+TEST( DocumentEditorFacade, drawASentenceOnScreen) {
+    // Given
+    DocumentEditorFacade documentEditorFacade(3, 40
+        , "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.");
+    documentEditorFacade.setColor(0, 7, 5, "red");
+    documentEditorFacade.setColor(0, 20, 4, "orange");
+    documentEditorFacade.setColor(0, 33, 5, "yellow");
+    documentEditorFacade.setColor(1, 7, 6, "green");
+    documentEditorFacade.setColor(1, 15, 11, "blue");
+    documentEditorFacade.setColor(1, 31, 4, "indigo");
+    documentEditorFacade.setColor(2, 12, 6, "violet");
+    // When
+
+    // Then
+
 }
