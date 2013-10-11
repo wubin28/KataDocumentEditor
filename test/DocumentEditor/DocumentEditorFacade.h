3cd94251f5ad589f3ab1fea113de6071464836b1
Ben Wu <wubin28@gmail.com>
2013-10-11 09:41:44 +0800

    My first hello world check.

---
+++ test/DocumentEditor/DocumentEditorFacade.h
@@ -0,0 +1,11 @@
+#ifndef DocumentEditorFacade_h
+#define DocumentEditorFacade_h
+
+class DocumentEditorFacade {
+public:
+    int sayOne() {
+        return 1;
+    }
+};
+
+#endif
---
+++ test/DocumentEditor/DocumentEditorFacadeTest.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitLite/TestHarness.h"
+#include "CppUnitLite/TestDefaultConfig.h"
+#include "DocumentEditorFacade.h"
+
+int main(int argc, char* argv[])
+{
+	CommandLineTestRunner::RunAllTests(argc, argv);
+	return 0;
+}
+
+TEST( DocumentEditorFacade, creation)
+{
+    DocumentEditorFacade documentEditorFacade;
+    CHECK ( 1 == documentEditorFacade.sayOne() );
+}
