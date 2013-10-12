23b7af331242fe08c043e36f020a1daf9a430fb1
Ben Wu <wubin28@gmail.com>
2013-10-12 13:59:51 +0800

    Added two extra spaces in the input string to work around the word splitting issue at the end of each line.

--- test/DocumentEditor/DocumentEditorFacadeTest.cpp
+++ test/DocumentEditor/DocumentEditorFacadeTest.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
 
 TEST( DocumentEditorFacade, drawScreen) {
     // Given
-    DocumentEditorFacade documentEditorFacade("Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.");
+    DocumentEditorFacade documentEditorFacade("Do not dwell in the past, do not dream  of the future, concentrate the mind on  the present moment.");
     documentEditorFacade.setColor(0, 7, 5, "red");
     documentEditorFacade.setColor(0, 20, 4, "orange");
     documentEditorFacade.setColor(0, 33, 5, "yellow");
@@ -23,17 +23,18 @@ TEST( DocumentEditorFacade, drawScreen) {
 
     // Then
     STRCMP_EQUAL("black", documentEditorFacade.getColor(0,0).c_str());
-    LONGS_EQUAL('D', documentEditorFacade.getChar(0,0));
-    std::string tempStr(1, documentEditorFacade.getChar(0,1));
-    STRCMP_EQUAL("o", tempStr.c_str());
+    STRCMP_EQUAL("D", std::string(1, documentEditorFacade.getChar(0,0)).c_str());
     STRCMP_EQUAL("black", documentEditorFacade.getColor(0,1).c_str());
     STRCMP_EQUAL("red", documentEditorFacade.getColor(0, 8).c_str());
     STRCMP_EQUAL("red", documentEditorFacade.getColor(0,9).c_str());
+    STRCMP_EQUAL("d", std::string(1, documentEditorFacade.getChar(0,7)).c_str());
     STRCMP_EQUAL("orange", documentEditorFacade.getColor(0, 20).c_str());
+    STRCMP_EQUAL("p", std::string(1, documentEditorFacade.getChar(0,20)).c_str());
     STRCMP_EQUAL("yellow", documentEditorFacade.getColor(0, 33).c_str());
+    STRCMP_EQUAL("d", std::string(1, documentEditorFacade.getChar(0,33)).c_str());
     STRCMP_EQUAL("green", documentEditorFacade.getColor(1, 7).c_str());
     STRCMP_EQUAL("blue", documentEditorFacade.getColor(1, 15).c_str());
     STRCMP_EQUAL("indigo", documentEditorFacade.getColor(1, 31).c_str());
     STRCMP_EQUAL("violet", documentEditorFacade.getColor(2, 12).c_str());
-
+    STRCMP_EQUAL("t", std::string(1, documentEditorFacade.getChar(2,17)).c_str());
 }
